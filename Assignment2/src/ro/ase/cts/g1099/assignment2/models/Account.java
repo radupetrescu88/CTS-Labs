7aa401431bd381b59c3a35fc1b7006c3386cf462
radupetrescu88 <petrescuradu19@stud.ase.ro>
2022-04-03 21:15:49 +0300

    2nd Commit - name conventions

--- Assignment2/src/ro/ase/cts/g1099/assignment2/models/Account.java
+++ Assignment2/src/ro/ase/cts/g1099/assignment2/models/Account.java
@@ -1,68 +1,67 @@
 package ro.ase.cts.g1099.assignment2.models;
 
 public class Account {
-	public double	loan_value,rate;	
-	public int	daysActive,account_Type;
-	public static final int	STANDARD=0,BUDGET=1,PREMIUM=2,SUPER_PREMIUM=3;
-	
-	public double loan() {
-		System.out.println("The loan value is " + this.loan_value);
-		return loan_value;
+	public double loanValueInRON, accountRate;
+	public int daysActive, accountType;
+	public static final int STANDARD = 0, BUDGET = 1, PREMIUM = 2, SUPER_PREMIUM = 3;
+
+	public Account(double loanValueInRON, double rate, int accountType) throws Exception {
+		if (loanValueInRON < 0) {
+			throw new Exception();
+		} else {
+			this.loanValueInRON = loanValueInRON;
+		}
+		this.accountRate = rate;
+		this.accountType = accountType;
 	}
-	
+
+	public double getLoanValueInRON() {
+		System.out.println("The loan value in RON is " + this.loanValueInRON);
+		return loanValueInRON;
+	}
+
 	public double getRate() {
-		System.out.println("The rate is "+rate);
-		return this.rate;
+		System.out.println("The rate is " + accountRate);
+		return this.accountRate;
 	}
-	
-	//must have method - the lead has requested it in all classes
+
+	// must have method - the lead has requested it in all classes
 	public double getMonthlyRate() {
-		return loan_value*rate;
-	}
-	
-	public void setValue(double value) throws Exception {
-		if(value<0)
-			throw new Exception();
-		else
-		{
-			loan_value = value;
-		}
-	}
-	
-	public String to_string() {
-		return "Loan: "+this.loan_value+"; rate: "+this.rate+"; days active:"+daysActive+"; Type: "+account_Type+";";
-	}
-	
-	public void print() {
-		int vb = 10;
-		System.out.println("This is an account");
+		return loanValueInRON * accountRate;
 	}
 
-	public static double calculate(Account[] 	accounts)
-	{
-	double totalFee=0.0;
-	Account	account;
-	int temp = 365;
-	for	(int	i=0;i<accounts.length;i++)	{
-	account=accounts[i];
-	if(account.account_Type==Account.PREMIUM||account.account_Type==Account.SUPER_PREMIUM)	
-	totalFee+=.0125	*	(	//	1.25%	broker's	fee
-			account.loan_value*Math.pow(account.rate,(account.daysActive/365)) - account.loan_value);	//	interest-principal
-	}
-	return	totalFee;
+	public void setLoanValue(double value) throws Exception {
+		if (value < 0)
+			throw new Exception();
+		else {
+			loanValueInRON = value;
+		}
 	}
 
-	public Account(double value, double rate, int account_Type) throws Exception {
-		if(value<0)
-			throw new Exception();
-		else
-		{
-			loan_value = value;
+	public static double calculateBrokerFee(Account[] accounts) {
+		double totalFee = 0.0;
+		Account account;
+		//int temp = 365;
+		for (int i = 0; i < accounts.length; i++) {
+			account = accounts[i];
+			if (account.accountType == Account.PREMIUM || account.accountType == Account.SUPER_PREMIUM) 
+				totalFee += .0125 * account.loanValueInRON * (Math.pow(account.accountRate, (account.daysActive / 365)-1));// interest-principal
 		}
-		this.rate = rate;
-		this.account_Type = account_Type;
+		return totalFee;
 	}
 	
-	
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Account [loanValueInRON=");
+		builder.append(loanValueInRON);
+		builder.append(", accountRate=");
+		builder.append(accountRate);
+		builder.append(", daysActive=");
+		builder.append(daysActive);
+		builder.append(", accountType=");
+		builder.append(accountType);
+		builder.append("]");
+		return builder.toString();
+	}
 }
-
